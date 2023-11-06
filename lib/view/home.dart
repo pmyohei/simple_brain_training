b9fe6578c623c77af3c8f1a5d451716ee6e58109
t.yohei <t.yohei@kandg.co.jp>
2023-11-06 19:37:24 +0900

    update

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -14,6 +14,14 @@ class Home extends ConsumerWidget {
     final theme = Theme.of(context);
 
     //------------------------
+    // UIサイズ
+    //------------------------
+    final deviceWidth = MediaQuery.of(context).size.width;
+    final startUISize = deviceWidth * 0.3;
+    final operationUISize = (deviceWidth * 0.7) / 4;
+    final operationUIRadius = operationUISize * 0.1;
+
+    //------------------------
     // 状態管理
     //------------------------
     // 状態監視：トレーニング情報
@@ -36,95 +44,127 @@ class Home extends ConsumerWidget {
       ),
       child: Scaffold(
         backgroundColor: Colors.transparent,
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: const Text('widget.title'),
-        ),
+        // appBar: AppBar(
+        //   backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        //   title: const Text('widget.title'),
+        // ),
         body: Center(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: <Widget>[
-              Text(
-                '脳トレについて色々記述',
-                style: Theme.of(context).textTheme.headlineMedium,
+              Container(
+                // margin: const EdgeInsets.fromLTRB(0, 40, 0, 0),
+                child: Text(
+                  '脳トレについて色々記述',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
               ),
               ElevatedButton(
                 onPressed: trainingInfoProvider.changeOnTraining,
                 style: ElevatedButton.styleFrom(
-                  minimumSize: const Size(100, 100),
+                  minimumSize: Size(startUISize, startUISize),
                   shape: const CircleBorder(),
+                  textStyle: const TextStyle(
+                    fontSize: 24,
+                  ),
                 ),
                 child: const Text('Start'),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  ElevatedButton(
-                    onPressed: () {
-                      trainingInfoProvider.setOperation(OperationSelector.plus);
-                    },
-                    style: ElevatedButton.styleFrom(
-                      fixedSize: const Size(40, 48),
-                      backgroundColor: trainingInfoProvider.operation ==
-                              OperationSelector.plus
-                          ? theme.colorScheme.primary
-                          : theme.colorScheme.onPrimary,
-                      foregroundColor: Colors.white,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(6),
+              Container(
+                // margin: const EdgeInsets.fromLTRB(0, 0, 0, 40),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    ElevatedButton(
+                      onPressed: () {
+                        trainingInfoProvider
+                            .setOperation(OperationSelector.plus);
+                      },
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(operationUISize, operationUISize),
+                        backgroundColor: trainingInfoProvider.operation ==
+                                OperationSelector.plus
+                            ? theme.colorScheme.primary
+                            : theme.colorScheme.onPrimary,
+                        foregroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius:
+                              BorderRadius.circular(operationUIRadius),
+                        ),
+                        textStyle: const TextStyle(
+                          fontSize: 32,
+                        ),
                       ),
+                      child: const Text('+'),
                     ),
-                    child: const Text('+'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      trainingInfoProvider
-                          .setOperation(OperationSelector.minus);
-                    },
-                    style: ElevatedButton.styleFrom(
-                        fixedSize: const Size(40, 40),
+                    ElevatedButton(
+                      onPressed: () {
+                        trainingInfoProvider
+                            .setOperation(OperationSelector.minus);
+                      },
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(operationUISize, operationUISize),
                         backgroundColor: trainingInfoProvider.operation ==
                                 OperationSelector.minus
                             ? theme.colorScheme.primary
                             : theme.colorScheme.onPrimary,
                         foregroundColor: Colors.white,
                         shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(2))),
-                    child: const Text('-'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      trainingInfoProvider
-                          .setOperation(OperationSelector.multiplication);
-                    },
-                    style: ElevatedButton.styleFrom(
-                        fixedSize: const Size(40, 40),
+                          borderRadius:
+                              BorderRadius.circular(operationUIRadius),
+                        ),
+                        textStyle: const TextStyle(
+                          fontSize: 32,
+                        ),
+                      ),
+                      child: const Text('-'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        trainingInfoProvider
+                            .setOperation(OperationSelector.multiplication);
+                      },
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(operationUISize, operationUISize),
                         backgroundColor: trainingInfoProvider.operation ==
                                 OperationSelector.multiplication
                             ? theme.colorScheme.primary
                             : theme.colorScheme.onPrimary,
                         foregroundColor: Colors.white,
                         shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(2))),
-                    child: const Text('×'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      trainingInfoProvider
-                          .setOperation(OperationSelector.division);
-                    },
-                    style: ElevatedButton.styleFrom(
-                        fixedSize: const Size(40, 40),
+                          borderRadius:
+                              BorderRadius.circular(operationUIRadius),
+                        ),
+                        textStyle: const TextStyle(
+                          fontSize: 32,
+                        ),
+                      ),
+                      child: const Text('×'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        trainingInfoProvider
+                            .setOperation(OperationSelector.division);
+                      },
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: Size(operationUISize, operationUISize),
                         backgroundColor: trainingInfoProvider.operation ==
                                 OperationSelector.division
                             ? theme.colorScheme.primary
                             : theme.colorScheme.onPrimary,
                         foregroundColor: Colors.white,
                         shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(2))),
-                    child: const Text('÷'),
-                  ),
-                ],
+                          borderRadius:
+                              BorderRadius.circular(operationUIRadius),
+                        ),
+                        textStyle: const TextStyle(
+                          fontSize: 32,
+                        ),
+                      ),
+                      child: const Text('÷'),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
--- lib/view/training.dart
+++ lib/view/training.dart
@@ -20,6 +20,15 @@ class Training extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final theme = Theme.of(context);
+
+    //------------------------
+    // UIサイズ
+    //------------------------
+    final deviceWidth = MediaQuery.of(context).size.width;
+    final trainingUISize = deviceWidth * 0.2;
+    final trainingUIRadius = trainingUISize * 0.1;
+
     //------------------------
     // 状態管理
     //------------------------
@@ -43,118 +52,161 @@ class Training extends HookConsumerWidget {
         case AppLifecycleState.resumed:
           print('LOG: バックグラウンドから復帰しました');
           _stopWatchTimer.onStartTimer();
-          break;
 
         case AppLifecycleState.inactive:
         case AppLifecycleState.paused:
         case AppLifecycleState.hidden:
           print('LOG: バックグラウンドになりました');
           _stopWatchTimer.onStopTimer();
-          break;
 
         case AppLifecycleState.detached:
           print('LOG: 終了しました');
           await _stopWatchTimer.dispose();
-          break;
       }
     });
 
     //------------------------
     // トレーニング画面
     //------------------------
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            //------------------------
-            // 画面上部
-            //------------------------
-            DecoratedBox(
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.yellow),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  const Text(
-                    'Training Time',
+    return DecoratedBox(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          stops: [0.1, 0.9],
+          colors: [
+            Color(0xffa8edea),
+            Color(0xfffed6e3),
+          ],
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              //------------------------
+              // 画面上部
+              //------------------------
+              Container(
+                margin: const EdgeInsets.fromLTRB(24, 0, 24, 40),
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                        color: const Color.fromARGB(255, 35, 35, 35)),
                   ),
-                  StreamBuilder<int>(
-                    stream: _stopWatchTimer.rawTime,
-                    initialData: _stopWatchTimer.rawTime.value,
-                    builder: (context, snapshot) {
-                      // ストップウオッチ時間を取得「hh:mm:ss.ms」
-                      final timerTime = StopWatchTimer.getDisplayTime(
-                        snapshot.data!,
-                      );
-                      // ms部分をカット：「hh:mm:ss.ms」⇒「hh:mm:ss」
-                      final displayTime = timerTime.substring(0, 8);
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(0, 8, 0, 8),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        const Text(
+                          'Training Time',
+                        ),
+                        StreamBuilder<int>(
+                          stream: _stopWatchTimer.rawTime,
+                          initialData: _stopWatchTimer.rawTime.value,
+                          builder: (context, snapshot) {
+                            // ストップウオッチ時間を取得「hh:mm:ss.ms」
+                            final timerTime = StopWatchTimer.getDisplayTime(
+                              snapshot.data!,
+                            );
+                            // ms部分をカット：「hh:mm:ss.ms」⇒「hh:mm:ss」
+                            final displayTime = timerTime.substring(0, 8);
 
-                      return Center(
-                        child: SizedBox(
-                          width: 144,
+                            return Padding(
+                              padding: const EdgeInsets.fromLTRB(0, 4, 0, 2),
+                              child: Text(
+                                displayTime,
+                                style: const TextStyle(fontSize: 24),
+                              ),
+                            );
+                          },
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.fromLTRB(24, 0, 24, 0),
+                          child: Divider(
+                            color: Color.fromARGB(255, 196, 19, 19),
+                          ),
+                        ),
+                        const Text(
+                          'Answers',
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 2, 0, 0),
                           child: Text(
-                            displayTime,
+                            answerCounter.value.toString(),
                             style: const TextStyle(fontSize: 24),
                           ),
                         ),
-                      );
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+
+              //------------------------
+              // 計算問題
+              //------------------------
+              Text(
+                quiestion1.value,
+                style: const TextStyle(
+                  fontSize: 44,
+                ),
+              ),
+              Text(
+                quiestion2.value,
+                style: const TextStyle(
+                  fontSize: 32,
+                ),
+              ),
+              Text(
+                quiestion3.value,
+                style: const TextStyle(
+                  fontSize: 24,
+                ),
+              ),
+              //------------------------
+              // 操作ボタン
+              //------------------------
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(
+                    onPressed: trainingInfoProvider.changeOnTraining,
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(trainingUISize, trainingUISize),
+                      shape: const CircleBorder(),
+                      textStyle: const TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                    child: const Text('Home'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      // 計算問題入れ替え
+                      quiestion1.value = quiestion2.value;
+                      quiestion2.value = quiestion3.value;
+                      quiestion3.value = Question().getFormula(operation);
+
+                      // 回答数カウント
+                      answerCounter.value++;
                     },
-                  ),
-                  const Divider(),
-                  const Text(
-                    'Answers',
-                  ),
-                  Text(
-                    answerCounter.value.toString(),
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(trainingUISize, trainingUISize),
+                      shape: const CircleBorder(),
+                      textStyle: const TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                    child: const Text('Next'),
                   ),
                 ],
               ),
-            ),
-
-            //------------------------
-            // 計算問題
-            //------------------------
-            Text(
-              quiestion1.value,
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            Text(
-              // counter.value.toString(),
-              quiestion2.value,
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            Text(
-              // counter.value.toString(),
-              quiestion3.value,
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            //------------------------
-            // 操作ボタン
-            //------------------------
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                  onPressed: trainingInfoProvider.changeOnTraining,
-                  child: const Text('Home'),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    // 計算問題入れ替え
-                    quiestion1.value = quiestion2.value;
-                    quiestion2.value = quiestion3.value;
-                    quiestion3.value = Question().getFormula(operation);
-
-                    // 回答数カウント
-                    answerCounter.value++;
-                  },
-                  child: const Text('Next'),
-                ),
-              ],
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );

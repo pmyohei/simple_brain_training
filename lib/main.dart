c663a9e8dedeb47c0d45db002cafbb66c08dbee3
t.yohei <t.yohei@kandg.co.jp>
2023-11-17 11:44:33 +0900

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 // Package imports:
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -10,7 +11,20 @@ import 'app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  //-----------
+  // Admob
+  //-----------
   MobileAds.instance.initialize();
-  // AdHelper.initialization();
-  runApp(const ProviderScope(child: SimpleBrainTrainingApp()));
+  //-----------
+  // 画面向き
+  //-----------
+  SystemChrome.setPreferredOrientations([
+    // 縦向きのみ許可
+    DeviceOrientation.portraitUp,
+  ]).then((_) {
+    //-----------
+    // App 起動
+    //-----------
+    runApp(const ProviderScope(child: SimpleBrainTrainingApp()));
+  });
 }
